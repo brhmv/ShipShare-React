c957d3415347ecb2bfdb46b03b356fcbfc207035
İslam Salamzadə <islam.salamzade.191@gmail.com>
2024-03-10 01:50:37 +0400

    chat page completed

--- src/components/SenderMessage.js
+++ src/components/SenderMessage.js
@@ -1,7 +1,25 @@
-const SenderMessage = ({text}) => {
+import { TiTick } from "react-icons/ti";
+
+const SenderMessage = ({msg}) => {
+
+    const dateString = msg.createdDate;
+    const date = new Date(dateString);
+    function getFormattedTime() {
+        const timeComponents = [date.getHours(), date.getMinutes()];
+        return timeComponents
+            .map(component => {
+                const pad = (component < 10) ? '0' : '';
+                return pad + component;
+            })
+            .join(':');
+    }
     return(
         <div className="sender-message">
-            {text}
+            {msg.text}
+            <div className="sender-info">
+                {getFormattedTime(date)}
+                <TiTick/>
+            </div>
         </div>
     );
 };
