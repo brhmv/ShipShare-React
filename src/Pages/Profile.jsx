aab8a4943925d3a256f563b1b5c6dd52fb94c746
brhmv <allahverdiibrahimov2004@gmail.com>
2024-03-19 00:04:53 +0400

    comme

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -53,6 +53,7 @@ const ProfileView = () => {
         if (postType === 'sender') {
             dispatch(deleteSenderPost(postId));
 
+
             toast.success("Sender Post deleted Succesfully!", {
                 position: "top-right",
             });
@@ -104,7 +105,7 @@ const ProfileView = () => {
                                 <p className="p-detail"><span className="span-detail">End Destination:</span> <span className='p-span-2'>{post.endDestination} <FaLocationDot /></span></p>
                                 <p className="p-detail"><span className="span-detail">Deadline Date:</span> {formatDate(post.deadlineDate)} <FaCalendarAlt /></p>
                                 <p className="p-detail"><span className="span-detail">Item Category: </span>{post.itemType}</p>
-                                <p className="p-detail"><span className="span-detail">Price:</span> {post.price} </p>
+                                <p className="p-detail"><span className="span-detail">Price:</span> {post.price} <FaDollarSign /></p>
                                 <p className="p-detail"><span className="span-detail">Weight:</span> {post.itemWeight} <GiWeight /></p>
                                 <p className="p-detail"><span className="span-detail">Views:</span> {post.views} <AiFillEye /></p>
 
@@ -136,7 +137,6 @@ const ProfileView = () => {
                         <div key={index} className="profile-post-item">
                             <div className="profile-post-details">
                                 <p className="p-detail"><span className="span-detail">Title:</span> <span className='p-span-2'>{post.title}</span></p>
-
                                 <p className="p-detail"><span className="span-detail">Description:</span> <span className='p-span-2'>{post.description}</span></p>
                                 <p className="p-detail"><span className="span-detail">Start Destination:</span> <span className='p-span-2'>{post.startDestination} <FaLocationDot /></span></p>
                                 <p className="p-detail"><span className="span-detail">End Destination:</span> <span className='p-span-2'>{post.endDestination} <FaLocationDot /></span></p>
@@ -158,6 +158,11 @@ const ProfileView = () => {
             </div >
         );
     };
+
+    ProfileView.defaultProps = {
+        myDetails: null,
+    };
+
 
     return (
         <div>
